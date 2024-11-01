2751d4e5053ce966a6a82b5f90f291d6e4ec89be
NityaGehlot <nityagehlot@gmail.com>
2024-11-01 14:35:31 -0500

    fantasy

--- nfl/src/App.js
+++ nfl/src/App.js
@@ -6,7 +6,7 @@ import NBAStandings from './NBAStandings';
 
 // Define simple components for each page
 const Home = () => <div className="content"><h2>Welcome to the Homepage!</h2></div>;
-const PageTwo = () => <div className="content"><h2>Page Two Content</h2></div>;
+const PageTwo = () => <div className="content"><h2>Welcome to the Fantasy Page!</h2></div>;
 const PageThree = () => <div className="content"><h2>Page Three Content</h2></div>;
 const PageFour = () => <div className="content"><h2>Page Four Content</h2></div>;
 
@@ -33,7 +33,7 @@ function App() {
                 {/* Navigation bar at the bottom with clickable sections */}
                 <nav className="navbar">
                     <Link to="/page1" className="nav-item"><span>NBA Standings</span></Link>
-                    <Link to="/page2" className="nav-item"><span>Section 2</span></Link>
+                    <Link to="/page2" className="nav-item"><span>NBA Fantasy</span></Link>
                     <Link to="/page3" className="nav-item"><span>Section 3</span></Link>
                     <Link to="/page4" className="nav-item"><span>Section 4</span></Link>
                 </nav>
